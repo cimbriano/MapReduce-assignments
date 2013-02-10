f8a3fd42af6bf59c5aa62b11074bd3bea9bb71a2
cimbriano <Christopher.Imbriano@gmail.com>
2013-02-09 19:32:02 -0500

    Adding empty PMI src files

---
+++ assignment2/src/main/PairsPMI.java
@@ -0,0 +1,21 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+
+public class PairsPMI extends Configured implements Tool {
+
+  @Override
+  public int run(String[] arg0) throws Exception {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
