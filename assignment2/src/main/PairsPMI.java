8fed2680ec364a2b791a63ad73be7e49a4ebf094
cimbriano <Christopher.Imbriano@gmail.com>
2013-02-10 11:24:45 -0500

    Corrected old Bigram name to PairsPMI. Added stubs and comments for desired mappers and reducers.  IO types need to be corrected

--- assignment2/src/main/PairsPMI.java
+++ assignment2/src/main/PairsPMI.java
@@ -28,10 +28,11 @@ import cern.colt.Arrays;
 
 public class PairsPMI extends Configured implements Tool {
 
-  private static final Logger LOG = Logger.getLogger(BigramFrequency.class);
+  private static final Logger LOG = Logger.getLogger(PairsPMI.class);
 
-  // Mapper: emits ... 
-  private static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+  // First stage Mapper: emits pairs (token, 1) for each unique token in an input value,
+  //                  and a pair (token pair, 1) for each unique pair of tokens
+  private static class CountsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     // Objects for reuse
     private final static IntWritable ONE = new IntWritable(1);
@@ -47,8 +48,8 @@ public class PairsPMI extends Configured implements Tool {
     }
   }
 
-  // Reducer: description ...
-  private static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+  // First stage Reducer: Totals counts for each Token and Token Pair
+  private static class CountsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     // Reuse objects
     private final static IntWritable SUM = new IntWritable();
 
@@ -58,6 +59,22 @@ public class PairsPMI extends Configured implements Tool {
 
     }
   }
+  
+  
+  
+  // Second stage mapper: Maps (A, TOTAL_A), (B, TOTAL_B) and (A_B, TOTAL_A_B) to the same reducer 
+  //                via a common key 
+  private static class TotalsMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    
+  }
+  
+  // Second Stage reducer: Finalizes PMI Calculation given 
+  private static class TotalsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    
+  }
+  
+  
+  
 
 
   public PairsPMI() {}
@@ -101,15 +118,15 @@ public class PairsPMI extends Configured implements Tool {
     int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ? 
         Integer.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
 
-        LOG.info("Tool: " + BigramCount.class.getSimpleName());
+        LOG.info("Tool: " + PairsPMI.class.getSimpleName());
         LOG.info(" - input path: " + inputPath);
         LOG.info(" - output path: " + outputPath);
         LOG.info(" - number of reducers: " + reduceTasks);
 
         Configuration conf = getConf();
         Job job = Job.getInstance(conf);
-        job.setJobName(BigramFrequency.class.getSimpleName());
-        job.setJarByClass(BigramFrequency.class);
+        job.setJobName(PairsPMI.class.getSimpleName());
+        job.setJarByClass(PairsPMI.class);
 
         job.setNumReduceTasks(reduceTasks);
 
@@ -122,9 +139,9 @@ public class PairsPMI extends Configured implements Tool {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        job.setMapperClass(MyMapper.class);
-        job.setCombinerClass(MyReducer.class);
-        job.setReducerClass(MyReducer.class);
+        job.setMapperClass(CountsMapper.class);
+        job.setCombinerClass(CountsReducer.class);
+        job.setReducerClass(CountsReducer.class);
 
         // Delete the output directory if it exists already.
         Path outputDir = new Path(outputPath);
