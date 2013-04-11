21142a5d7978795560af23fda98da6f98783faec
cimbriano <Christopher.Imbriano@gmail.com>
2013-04-10 21:54:46 -0400

    Basic 'tweet count' with no optimization

--- assignment6/src/main/ExtractHourlyCountsAll.java
+++ assignment6/src/main/ExtractHourlyCountsAll.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
@@ -14,19 +16,63 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
 import cern.colt.Arrays;
-import edu.umd.cloud9.io.pair.PairOfStringInt;
 
 public class ExtractHourlyCountsAll extends Configured implements Tool {
   private static final Logger LOG = Logger.getLogger(ExtractHourlyCountsAll.class);
   
-  public static class TweetsMapper extends Mapper<LongWritable, Text, PairOfStringInt, IntWritable>{
+  public static class AllTweetsMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+    
+//    public static final PairOfStringInt DATE_HOUR= new PairOfStringInt();
+    public static final Text DATE_HOUR = new Text();
+    public static final IntWritable ONE = new IntWritable(1);
+        
+    @Override
+    public void map(LongWritable key, Text text, Context context) throws IOException, InterruptedException {
+      
+      String[] rawTweet = text.toString().split("\t");
+      
+      String tweetID  = rawTweet[0];
+      String[] rawDateTime = rawTweet[1].split(" ");
+      
+      String month = rawDateTime[0].equals("Jan") ? "01" : "02";
+      String day = rawDateTime[2];
+      String hour = rawDateTime[3].split(":")[0];
+      
+      String userID = rawTweet[2];
+      String tweetText = rawTweet[3];
+      
+      DATE_HOUR.set( month + day + hour);
+      
+      context.write(DATE_HOUR, ONE);
+      
+    }
+  }
+  
+  public static class AllTweetsReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+    
+    public static final IntWritable COUNT = new IntWritable();
+    
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+      int total = 0;
+      
+      for(IntWritable value : values){
+        total += value.get();
+      }
+      
+      COUNT.set(total);
+      
+      context.write(key, COUNT);
+    }
     
   }
   
@@ -47,65 +93,67 @@ public class ExtractHourlyCountsAll extends Configured implements Tool {
    */
   @SuppressWarnings({ "static-access" })
   public int run(String[] args) throws Exception {
-    Options options = new Options();
+//    Options options = new Options();
+//
+//    options.addOption(OptionBuilder.withArgName("path").hasArg()
+//        .withDescription("input path").create(INPUT));
+//    options.addOption(OptionBuilder.withArgName("path").hasArg()
+//        .withDescription("output path").create(OUTPUT));
+//    options.addOption(OptionBuilder.withArgName("num").hasArg()
+//        .withDescription("number of reducers").create(NUM_REDUCERS));
+//
+//    CommandLine cmdline;
+//    CommandLineParser parser = new GnuParser();
+//
+//    try {
+//      cmdline = parser.parse(options, args);
+//    } catch (ParseException exp) {
+//      System.err.println("Error parsing command line: " + exp.getMessage());
+//      return -1;
+//    }
+//
+//    if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
+//      System.out.println("args: " + Arrays.toString(args));
+//      HelpFormatter formatter = new HelpFormatter();
+//      formatter.setWidth(120);
+//      formatter.printHelp(this.getClass().getName(), options);
+//      ToolRunner.printGenericCommandUsage(System.out);
+//      return -1;
+//    }
+//
+//    String inputPath = cmdline.getOptionValue(INPUT);
+//    String outputPath = cmdline.getOptionValue(OUTPUT);
+//    int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ?
+//        Integer.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
 
-    options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("input path").create(INPUT));
-    options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("output path").create(OUTPUT));
-    options.addOption(OptionBuilder.withArgName("num").hasArg()
-        .withDescription("number of reducers").create(NUM_REDUCERS));
-
-    CommandLine cmdline;
-    CommandLineParser parser = new GnuParser();
-
-    try {
-      cmdline = parser.parse(options, args);
-    } catch (ParseException exp) {
-      System.err.println("Error parsing command line: " + exp.getMessage());
-      return -1;
-    }
-
-    if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
-      System.out.println("args: " + Arrays.toString(args));
-      HelpFormatter formatter = new HelpFormatter();
-      formatter.setWidth(120);
-      formatter.printHelp(this.getClass().getName(), options);
-      ToolRunner.printGenericCommandUsage(System.out);
-      return -1;
-    }
-
-    String inputPath = cmdline.getOptionValue(INPUT);
-    String outputPath = cmdline.getOptionValue(OUTPUT);
-    int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ?
-        Integer.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
-
+    
+    String inputPath = "";
+    String outputPath = "";
+    
     LOG.info("Tool: " + ExtractHourlyCountsAll.class.getSimpleName());
     LOG.info(" - input path: " + inputPath);
     LOG.info(" - output path: " + outputPath);
-    LOG.info(" - number of reducers: " + reduceTasks);
 
     Configuration conf = getConf();
     Job job = Job.getInstance(conf);
     job.setJobName(ExtractHourlyCountsAll.class.getSimpleName());
     job.setJarByClass(ExtractHourlyCountsAll.class);
 
-    job.setNumReduceTasks(reduceTasks);
 
     FileInputFormat.addInputPath(job, new Path(inputPath));
     FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
-    // job.setInputFormatClass(TextInputFormat.class);
-    // job.setOutputFormatClass(TextOutputFormat.class);
+     job.setInputFormatClass(TextInputFormat.class);
+     job.setOutputFormatClass(TextOutputFormat.class);
 
+     job.setMapOutputKeyClass(Text.class);
+     job.setMapOutputValueClass(IntWritable.class);
 
-    // job.setMapOutputKeyClass(IntWritable.class);
-    // job.setMapOutputValueClass(PageRankNodeExtended.class);
+     job.setOutputKeyClass(Text.class);
+     job.setOutputValueClass(IntWritable.class);
 
-    // job.setOutputKeyClass(IntWritable.class);
-    // job.setOutputValueClass(PageRankNodeExtended.class);
-
-    // job.setMapperClass(MyMapper.class);
+     job.setMapperClass(AllTweetsMapper.class);
+     job.setReducerClass(AllTweetsReducer.class);
 
 
     // Delete the output directory if it exists already.
