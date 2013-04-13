fef9ba0be07d912022f8f56b51173f452e846803
cimbriano <Christopher.Imbriano@gmail.com>
2013-04-12 23:38:12 -0400

    Tweaked

--- assignment6/make_fake_tweet_data.rb
+++ assignment6/make_fake_tweet_data.rb
@@ -6,8 +6,7 @@ File.open("tweets2011.txt", 'w') do |file|
 
 		# Ugly way to make a tweet id from 5 random ints up to 100
 		tweet_id = Array.new(5){rand 100}.map {|e| e.to_s}.inject(""){ |acc, ele| acc << ele }
-		date = "#{/(Sun|Mon|Tue|Wed|Thu|Fri|Sat)/.gen} #{/(Jan)/.gen}  #{/\d/.gen}0:00:00 +0000 2011"
-		# date = "#{/(Sun|Mon|Tue|Wed|Thu|Fri|Sat)/.gen} #{/(Jan|Feb)/.gen} #{/\d{2}/.gen} #{/\d{2}/.gen}:#{/\d{2}/.gen}:#{/\d{2}/.gen} +0000 2011"
+		date = "#{/(Sun|Mon|Tue|Wed|Thu|Fri|Sat)/.gen} #{/(Jan|Feb)/.gen} #{/\d{2}/.gen} #{/\d{2}/.gen}:#{/\d{2}/.gen}:#{/\d{2}/.gen} +0000 2011"
 		user_name = Faker::Name.last_name.downcase
 		tweet_text = Faker::Lorem.sentence
 
@@ -15,6 +14,7 @@ File.open("tweets2011.txt", 'w') do |file|
 		file.write(date + "\t")
 		file.write(user_name + "\t")
 		file.write(tweet_text)
+		file.write("EgyPt") if false
 		file.write("\n")
 
 	end	
